3cc521145873ee1f803a7f06edc9560bde30a6f3
LuisJDomingo <luisdomingogarcia79@gmail.com>
2025-06-08 21:22:56 +0200

    update

--- random_code.py
+++ random_code.py
@@ -1,10 +1,10 @@
-print('Actualización aleatoria')
-
 def bar():
     return 42
 
-def greet():
-    print('Hola desde GitHub!')
+print('Actualización aleatoria')
 
 def foo():
     return 'foo'
+
+def greet():
+    print('Hola desde GitHub!')
